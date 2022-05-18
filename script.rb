88c864ee2ac02fc06e8eb217da5239e0ad2f146c
Eduardo Carneiro <eduardo.carneiro2907@gmail.com>
2022-05-17 21:03:52 -0300

    Add round terminal interface
    At now, it doesnt do anyting with the given input.
    
    Also refactored start_game function

--- script.rb
+++ script.rb
@@ -1,11 +1,10 @@
 class Game
-  attr_accessor :board
-
   def initialize
-    @board = Array.new(3) { Array.new(3) }
+    @board = Array.new(3) { Array.new(3, ' ') }
+    start_game
   end
 
-  def start_game
+  def create_players
     @players = Array.new(2) { nil }
     @players.length.times do |i|
       puts "Hello Player #{i + 1}! Insert your name below here:"
@@ -13,16 +12,50 @@ class Game
       @players[i] = Player.new(name)
     end
   end
+
+  def start_game
+    create_players
+    round = Round.new(@players, @board)
+    round.print_round
+  end
 end
 
 class Player
-  def initialize(name) 
+  attr_reader :name
+  def initialize(name)
     @name = name
   end
 end
 
 class Round
+  def initialize(players, board)
+    @players = players
+    @board = board
+  end
+
+  def show_board
+    string_board = ''
+    @board.each do |row|
+      row.each do  |position|
+        string_board += " #{position} |"
+      end
+      string_board.delete_suffix!('|')
+      string_board += "\n---+---+---\n"
+    end
+    string_board.delete_suffix!("\n---+---+---\n")
+  end
+
+  def get_coordinate
+    puts 'Type the coordinate of your markdown here: '
+    coordinate = gets.chomp
+    coordinate
+  end
+
+  def print_round
+    puts "Round x\n"
+    puts "#{show_board}\n"
+    get_coordinate
+  end
 end
 
-new_game = Game.new
-new_game.start_game
+Game.new
